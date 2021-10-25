f78562265be0a291b42a33694458bbfad479fa2c
Kerim Satirli <k.satirli@gmail.com>
2021-10-25 13:06:30 +0200

    rename `citrix` to `converge`

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,14 +1,14 @@
 ## see https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/image
-data "azurerm_image" "citrix" {
+data "azurerm_image" "converge" {
   name                = local.resource_group_name
   resource_group_name = local.resource_group_name
 }
 
 # see https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine
-resource "azurerm_linux_virtual_machine" "citrix" {
-  name                = data.azurerm_image.citrix.name
-  resource_group_name = data.azurerm_image.citrix.resource_group_name
-  location            = data.azurerm_image.citrix.location
+resource "azurerm_linux_virtual_machine" "converge" {
+  name                = data.azurerm_image.converge.name
+  resource_group_name = data.azurerm_image.converge.resource_group_name
+  location            = data.azurerm_image.converge.location
   size                = "Standard_F2"
 
   admin_username = "adminuser"
@@ -21,7 +21,7 @@ resource "azurerm_linux_virtual_machine" "citrix" {
 
   # see https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine#network_interface_ids
   network_interface_ids = [
-    azurerm_network_interface.citrix.id,
+    azurerm_network_interface.converge.id,
   ]
 
   # see https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine#caching
@@ -33,5 +33,5 @@ resource "azurerm_linux_virtual_machine" "citrix" {
   # The value for `source_image_id` is dynamically retrieved through the data source
   # at the top of this file; this allows for less maintenance and a more robust system
   # see https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine#source_image_id
-  source_image_id = data.azurerm_image.citrix.id
+  source_image_id = data.azurerm_image.converge.id
 }
